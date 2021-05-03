942ceb9452b7b828c4319dced65c953a3c913b89
Murunga Kibaara <murungakibaara@outlook.com>
2021-05-03 23:34:48 +0300

    [Finish Users Page]

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -4,6 +4,7 @@ import {
     StyleSheet,
     FlatList,
     ActivityIndicator,
+    ScrollView
    } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { List, Divider } from 'react-native-paper';
@@ -19,9 +20,9 @@ const Users = () => {
 
     const renderRow = ({ item }) => (
         <List.Item
-          style={{ padding: 3 }}
-          title={item.name}
-          description={item.email}
+          style={{ padding: 4 }}
+          title={item?.username}
+          description={item?.name}
         //   left={(props) => (
         //     <List.Icon {...props} size={30} icon="account" color="#c4c4c4" />
         //   )}
@@ -37,25 +38,27 @@ const Users = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            {
-                users?.loading
-                ? <ActivityIndicator 
-                    size={35}
-                    color="#000000"
-                    style={{ marginTop: 150, alignSelf: 'center' }}
-                />
-                : <FlatList
-                    data={users?.data}
-                    renderItem={renderRow}
-                    keyExtractor={(item, index) => index.toString()}
-                    onEndReachedThreshold={7}
-                    initialNumToRender={7}
-                    removeClippedSubviews={true}
-                    maxToRenderPerBatch={10}
-                    windowSize={10}
-                    ItemSeparatorComponent={() => <Divider />}
-                />
-            }
+            <ScrollView>
+                {
+                    users?.loading
+                    ? <ActivityIndicator 
+                        size={35}
+                        color="#000000"
+                        style={{ marginTop: 150, alignSelf: 'center' }}
+                    />
+                    : <FlatList
+                        data={users?.data}
+                        renderItem={renderRow}
+                        keyExtractor={(item, index) => index.toString()}
+                        onEndReachedThreshold={7}
+                        initialNumToRender={7}
+                        removeClippedSubviews={true}
+                        maxToRenderPerBatch={10}
+                        windowSize={10}
+                        ItemSeparatorComponent={() => <Divider />}
+                    />
+                }
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -64,8 +67,8 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
     },
 });
 
